bbdb0f7a2d1ec2b64dd783368bf8258f3fbc1b16
Praneet Kumar Dutta <duttapraneet@gmail.com>
2025-11-06 20:26:49 +0530

    card

---
+++ test/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const card = () => {
+  return (
+    <div className="card">
+      <div>
+        <div className="top">
+            <h3>120$/hr</h3>
+        </div>
+        <div className="middle"></div>
+      </div>
+      <div className="bottom"></div>
+    </div>
+  );
+};
+
+export default card;
